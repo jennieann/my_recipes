fd05632f32db233b62c18c8b85b337157492dd88
jennieann <mail@jennie-ann.se>
2017-02-28 15:32:28 +0100

    Add title functionality for category show page

--- spec/features/creating_categories_spec.rb
+++ spec/features/creating_categories_spec.rb
@@ -12,6 +12,14 @@ RSpec.feature "User can create new categories" do
 
         expect(page).to have_content "Category has been created."
 
+        category = Category.find_by(name: "Desserts")
+        expect(page.current_url).to eq category_url(category)
+
+        title = "Desserts"
+        expect(page).to have_title title
+
+
+
     end
 
 end
